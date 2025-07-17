2aff42460fc64e5f0696eafa5d8b6b78c9bac537
Maxi <adekanyesemilore91@gmail.com>
2025-07-17 01:31:11 +0100

    update

--- src/components/dasboardelements/Navigation.tsx
+++ src/components/dasboardelements/Navigation.tsx
@@ -2,7 +2,7 @@ import { LayoutDashboardIcon } from "lucide-react"
 
 const Navigation = () => {
   return (
-    <div className="flex justify-around backdrop-blur-lg fixed bottom-0 py-3 bg-white/30 w-full b">
+    <div className="flex justify-around backdrop-blur-lg fixed bottom-0 pt-3 pb-6 bg-white/30 w-full b">
             <LayoutDashboardIcon/>
             <LayoutDashboardIcon/>
             <LayoutDashboardIcon/>
