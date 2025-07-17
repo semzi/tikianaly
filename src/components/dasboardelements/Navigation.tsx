68760844923a5c7091722fc4c4cae97b4ead01bf
Maxi <adekanyesemilore91@gmail.com>
2025-07-17 01:48:29 +0100

    update

--- src/components/dasboardelements/Navigation.tsx
+++ src/components/dasboardelements/Navigation.tsx
@@ -1,12 +1,16 @@
+import { HomeIcon } from "@heroicons/react/20/solid"
+import { UserCircleIcon } from "@heroicons/react/24/outline"
+import { BookOpenIcon } from "@heroicons/react/24/outline"
+import { TrophyIcon } from "@heroicons/react/24/outline"
 import { LayoutDashboardIcon } from "lucide-react"
 
 const Navigation = () => {
   return (
-    <div className="flex justify-around backdrop-blur-lg fixed bottom-0 pt-3 pb-6 bg-white/30 w-full b">
-            <LayoutDashboardIcon/>
-            <LayoutDashboardIcon/>
-            <LayoutDashboardIcon/>
-            <LayoutDashboardIcon/>
+    <div className="flex justify-around backdrop-blur-lg fixed bottom-0 pt-3 pb-5 bg-white/30 w-full b">
+            <HomeIcon className="h-7 w-7" />
+            <TrophyIcon className="h-7 w-7" />
+            <BookOpenIcon className="h-7 w-7" />
+            <UserCircleIcon className="h-7 w-7" />
     </div>
   )
 }
