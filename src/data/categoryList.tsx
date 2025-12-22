79fd5325b95205f1e51697c0119d10258c4220b2
oreayo <adekanyesemilore91@gmail.com>
2025-12-22 03:33:35 -0800

    category

--- src/data/categoryList.tsx
+++ src/data/categoryList.tsx
@@ -1,12 +1,8 @@
 // Category button data for the dashboard
 
 const categories = [
-  { label: "All Sports", variant: "outline", href: "" },
   { label: "Football", variant: "primary", href: "/dashboard/football" },
-  { label: "Cricket", variant: "outline", href: "/dashboard/cricket" },
-  { label: "Basketball", variant: "outline", href: "/dashboard/basketball" },
-  { label: "Tennis", variant: "outline", href: "/dashboard/tennis" },
-  { label: "Baseball", variant: "outline", href: "/dashboard/baseball" },
+  { label: "Other Sports Coming Soon..", variant: "outline", href: "" }
 ];
 
 export default categories;
